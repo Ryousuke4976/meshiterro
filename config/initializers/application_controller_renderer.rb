b13981a93b1c65649d18542a1f316a2517130e94
Ryousuke4976 <satou.ryousuke0503@gmail.com>
2019-09-21 18:19:09 +0900

    [Add] コメント機能といいね機能と画像表示数の制限機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7628168c2c91b007fc3a3a57973c283e0d98b749367fee2701314c033d6293093723ad66fe16a28ac16affdeb1ef876c055fb8a03ab1d652d930917829e93bf8'
